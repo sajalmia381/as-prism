f5936e7dc945ae829b32e55a3d5beb52b3d7ff68
Md Sajal Mia <sajalmia381@gmail.com>
2023-03-13 09:57:11 +0600

    path fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AsPrismModule } from 'projects/as-prism/src/public-api';
+import { AsPrismModule } from 'as-prism';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
