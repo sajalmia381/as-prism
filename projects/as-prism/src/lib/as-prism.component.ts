f6e42bd790d8ab8ab5f9726c39dd02888212bf39
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-11 14:59:02 +0600

    whitespace bug fix

--- projects/as-prism/src/lib/as-prism.component.ts
+++ projects/as-prism/src/lib/as-prism.component.ts
@@ -1,19 +1,18 @@
 import { AfterViewInit, Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 import { AsPrismService } from './as-prism.service';
+
 @Component({
   selector: 'as-prism',
   template: `
     <pre *ngIf="language" [class]="'language-' + language">
       <code #codeEle [class]="'language-' + language">{{code}}</code>
     </pre>
-  `,
-  styles: [
-  ]
+  `
 })
-export class AsPrismComponent implements AfterViewInit, OnChanges {
+export class AsPrismComponent implements OnChanges, AfterViewInit {
   @ViewChild('codeEle') codeEle!: ElementRef;
+  
   @Input() code!: string;
-
   @Input() language: string = 'json';
 
   constructor(private prismService: AsPrismService) { }
--- projects/as-prism/src/lib/as-prism.service.ts
+++ projects/as-prism/src/lib/as-prism.service.ts
@@ -1,15 +1,14 @@
 import { ElementRef, Injectable } from '@angular/core';
 import * as Prism from "prismjs";
 import { AsPrismOptions } from './as-prism.interface';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
 @Injectable()
 export class AsPrismService {
-
-  constructor() { }
-
-  /*
-  * @param {el: ElementRef, option: AsPrismOptions}
-  */
+  /**
+   * @param {ElementRef} el - HTMLELement
+   * @param {AsPrismOptions} [options] - Highligher Option
+   */
   public highlightElement(el: ElementRef, options?: AsPrismOptions): void {
     if (el instanceof ElementRef) {
       Prism.highlightElement(el.nativeElement, options?.async, options?.callback);
